37fb8efbf80a89702d4f6d73976d61922e3458cd
Tony <sonictony@gmail.com>
2023-04-13 12:59:55 -0800

    Adding Peacock class

---
+++ Peacock.java
@@ -0,0 +1,14 @@
+// Name: Tony Coraggio
+// Date: 4/19/2023
+// Description: Peacock class that extends the Animal class
+
+public class Peacock extends Animal {
+   public abstract void sound() {
+      System.out.println(name + " makes the noise: 'SQUAHHHH!!'");
+   }
+   
+   public abstract void play() {
+      System,out.println(name + " likes to flaunt their pretty feathers");
+   }
+
+}
