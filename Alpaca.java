0d1435fea7381d5ec377119fa019775f1513ddbe
Issa-73 <as.issa73@gmail.com>
2023-04-13 16:44:46 -0700

    add coments and java docs

--- Alpaca.java
+++ Alpaca.java
@@ -1,8 +1,12 @@
+/**
+ * Alpaca class ettends the Animal Class
+ * @author Issa
+ */
+
 public class Alpaca extends Animal {
 
     /**
      * A constructor that takes in a name.
-     *
      * @param name Name of the animal.
      */
     public Alpaca(String name) {
@@ -14,18 +18,25 @@ public class Alpaca extends Animal {
      * an animal makes.
      */
     @Override
+
     public void sound() {
+        // print how Alpaca sounds like
+        System.out.println(this.name + " sound like   mmmaammmi  .");}
 
-
-        System.out.println(this.name + " sound like   mmmaammmi  .");
-    }
-
+    /**
+     * Abstract method that should represent a specific sound
+     *   an animal makes
+     */
     public void play(){
+        // print what Alpaca plays.
         System.out.println(this.name + " plays soccer ball lol.");
-
-}
-
+    }
+    /**
+     * Abstract method that should represent a specific sound
+     *   an animal makes
+     */
     public void eat(){
+        // print what Alpaca eats
         System.out.println(this.name + " eats some grains.");
     }
 
