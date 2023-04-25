c091cd41d2c2b8eda7800ad68394bc27f0323b3d
Bishal Kshetri <47649491+bsaaksh@users.noreply.github.com>
2023-04-25 15:43:54 -0700

    play method added

--- Chipmunk.java
+++ Chipmunk.java
@@ -8,4 +8,9 @@ public class Chipmunk extends Animal {
    public void sound(){
       System.out.println(this.name + " does chucking when there is any threat.");
    } 
+   
+   @Override
+   public void play(){
+      System.out.println(this.name + " plays around in the forest.");
+   }
 }
