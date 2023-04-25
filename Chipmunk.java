62953b24717462734de72a1d251fe612271fbe8d
Bishal Kshetri <47649491+bsaaksh@users.noreply.github.com>
2023-04-25 15:42:02 -0700

    sound method added

--- Chipmunk.java
+++ Chipmunk.java
@@ -3,4 +3,9 @@ public class Chipmunk extends Animal {
    public Chipmunk(String name){
       super(name);
    }
+
+   @Override
+   public void sound(){
+      System.out.println(this.name + " does chucking when there is any threat.");
+   } 
 }
