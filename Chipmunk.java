01f7152ec72a3c4e7c918aac06a7199d29cd32bf
Bishal Kshetri <47649491+bsaaksh@users.noreply.github.com>
2023-04-25 15:45:51 -0700

    eat method added

--- Chipmunk.java
+++ Chipmunk.java
@@ -13,4 +13,9 @@ public class Chipmunk extends Animal {
    public void play(){
       System.out.println(this.name + " plays around in the forest.");
    }
+   
+   @Override
+   public void eat(){
+      System.out.println(this.name + " eats fruits and nuts.");
+   } 
 }
